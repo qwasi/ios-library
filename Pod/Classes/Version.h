a6f07037be8c7dfdee4f6a6331de4305401c2f98
administrator <administrator@Qwasi-Build.local>
2015-08-04 11:38:22 -0700

    #bump Release 2.1.14

--- Pod/Classes/Version.h
+++ Pod/Classes/Version.h
@@ -1,4 +1,4 @@
 // Version Header
 #define SHORT_VERSION @"2.1.14"
-#define BUILD_VERSION 119
-#define VERSION_STRING @"2.1.14-119" 
+#define BUILD_VERSION 1000
+#define VERSION_STRING @"2.1.14-1000" 
