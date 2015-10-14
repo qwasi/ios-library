b7362f8ce7d0cf829a22ff04caefccb3877b84f7
Qwasi Build <support@qwasi.com>
2015-10-13 18:03:47 -0700

    #bump Release 2.1.16-dev.152

--- Pod/Classes/Version.h
+++ Pod/Classes/Version.h
@@ -1,4 +1,4 @@
 // Version Header
 #define SHORT_VERSION @"2.1.16"
-#define BUILD_VERSION 151
-#define VERSION_STRING @"2.1.16-dev.151" 
+#define BUILD_VERSION 152
+#define VERSION_STRING @"2.1.16-dev.152" 
