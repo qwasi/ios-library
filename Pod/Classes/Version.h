5f67d78c146eeaca5120c97d8419faf93f34a396
Rob Rodriguez <rob@qwasi.com>
2015-12-23 16:34:22 -0800

    #bump Release 2.1.20-dev.180

--- Pod/Classes/Version.h
+++ Pod/Classes/Version.h
@@ -1,4 +1,4 @@
 // Version Header
-#define SHORT_VERSION @"2.1.19"
-#define BUILD_VERSION 179
-#define VERSION_STRING @"2.1.19-dev.179" 
+#define SHORT_VERSION @"2.1.20"
+#define BUILD_VERSION 180
+#define VERSION_STRING @"2.1.20-dev.180" 
